485db27a6180db7e0d55228cf0264f761b1a9d6a
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-11-26 14:16:37 +0600

    Update scripts.sh

--- web-platform_22_11_dev/web/scripts/scripts.sh
+++ web-platform_22_11_dev/web/scripts/scripts.sh
@@ -1,194 +1,3 @@
-############################################
-# виртуальное окружение и наследование глобальных пакетов
-virtualenv --system-site-packages mycoolproject
-############################################
-
-#########################################
-# запуск скрипта
-python -m pdb my_script.py
-#########################################
-
-############################################
-# генераторы
-from contextlib import contextmanager
-
-@contextmanager
-def open_file(name):
-    f = open(name, 'w')
-    yield f
-    f.close()
-###############################################
-
-############################################
-# else в циклах
-for n in range(2, 10):
-    for x in range(2, n):
-        if n % x == 0:
-            print( n, 'equals', x, '*', n/x)
-            break
-    else:
-        # Цикл не нашел целочисленного делителя для n
-        print(n, 'is a prime number')
-########################################
-
-####################################
-# отладчик python
-import pdb
-
-def make_bread():
-    pdb.set_trace()
-    return "У меня нет времени"
-
-print(make_bread())
-######################################
-
-###########################################
-# тернарные операторы
-is_nice = True
-state = "nice" if is_nice else "not nice"
-############################################
-
-#######################################################
-# распаковка
-def profile():
-    name = "Danny"
-    age = 30
-    return name, age
-
-profile_name, profile_age = profile()
-print(profile_name)
-# Вывод: Danny
-
-print(profile_age)
-# Вывод: 30
-#####################################################
-
-
-###########################################################
-# магические переменные *args и **kwargs
-def test_var_args(f_arg, *argv):
-    print("Первый позиционный аргумент:", f_arg)
-    for arg in argv:
-        print("Другой аргумент из *argv:", arg)
-
-test_var_args('yasoob', 'python', 'eggs', 'test')
-
-
-def greet_me(**kwargs):
-    for key, value in kwargs.items():
-        print("{0} = {1}".format(key, value))
-
->>> greet_me(name="yasoob")
-name = yasoob
-###########################################################
-
-######################################################
-# enumerate
-my_list = ['apple', 'banana', 'grapes', 'pear']
-for c, value in enumerate(my_list, 1):
-    print(c, value)
-
-# Вывод:
-# 1 apple
-# 2 banana
-# 3 grapes
-# 4 pear
-
-
-my_list = ['apple', 'banana', 'grapes', 'pear']
-counter_list = list(enumerate(my_list, 1))
-print(counter_list)
-# Вывод: [(1, 'apple'), (2, 'banana'), (3, 'grapes'), (4, 'pear')]
-####################################################
-
-###################################################
-# map
-items = [1, 2, 3, 4, 5]
-squared = list(map(lambda x: x**2, items))
-
-
-def multiply(x):
-    return (x*x)
-def add(x):
-    return (x+x)
-
-funcs = [multiply, add]
-for i in range(5):
-    value = list(map(lambda x: x(i), funcs))
-    print(value)
-
-# Вывод:
-# [0, 0]
-# [1, 2]
-# [4, 4]
-# [9, 6]
-# [16, 8]
-####################################################
-
-####################################################
-# filter
-number_list = range(-5, 5)
-less_than_zero = list(filter(lambda x: x < 0, number_list))
-print(less_than_zero)
-
-# Вывод: [-5, -4, -3, -2, -1]
-######################################################
-
-######################################################
-#
-product = 1
-list = [1, 2, 3, 4]
-for num in list:
-    product = product * num
-
-# product = 24
-
-
-from functools import reduce
-product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
-
-# Вывод: 24
-##################################################
-
-##################################################
-# generator
-def generator_function():
-    for i in range(10):
-        yield i
-
-for item in generator_function():
-    print(item)
-
-# Вывод: 0
-# 1
-# 2
-# 3
-# 4
-# 5
-# 6
-# 7
-# 8
-# 9
-##################################################
-
-###################################################
-# корутины
-def grep(pattern):
-    print("Searching for", pattern)
-    while True:
-        line = (yield)
-        if pattern in line:
-            print(line)
-search = grep('coroutine')
-next(search)
-# Вывод: Searching for coroutine
-search.send("I love you")
-search.send("Don't you love me?")
-search.send("I love coroutines instead!")
-# Вывод: I love coroutines instead!
-####################################################
-
-
 
 ########################################################################################################################
 LINUX
@@ -210,14 +19,23 @@ deb http://http.us.debian.org/debian/ testing non-free contrib main
 
 sudo apt update -y
 sudo apt-get update -y
-sudo apt upgrade -y
-sudo apt-get upgrade -y
 sudo apt autoremove -y
+
+# clear cache
+sudo apt autoremove -y
+
+# clear terminal
+clear
+
+# удаление файла
+sudo rm temp.txt
+
+# редактирование / создание файла
+nano new.json
 
 # vbox
 !vbox insert guest-additions
-sudo apt install -y build-essential dkms linux-headers-$(uname -r)
-sudo apt-get install build-essential linux-headers-`uname -r` dkms virtualbox-dkms
+sudo apt install -y build-essential dkms virtualbox-dkms linux-headers-$(uname -r) linux-headers-`uname -r` dkms
 sudo mkdir -p /mnt/cdrom
 sudo mount /dev/cdrom /mnt/cdrom
 cd /mnt/cdrom
@@ -249,6 +67,122 @@ nvm use 18.10.0
 # extra
 
 ########################################################################################################################
+
+# update os
+sudo apt update -y
+sudo apt-get update -y
+sudo apt upgrade -y
+sudo apt-get upgrade -y
+
+
+# vbox
+#!vbox insert guest-additions
+sudo apt install -y build-essential dkms virtualbox-dkms linux-headers-$(uname -r) linux-headers-`uname -r` dkms
+sudo mkdir -p /mnt/cdrom
+sudo mount /dev/cdrom /mnt/cdrom
+cd /mnt/cdrom
+sudo sh ./VBoxLinuxAdditions.run --nox11
+sudo adduser user vboxsf
+sudo reboot
+
+# установка удалённого доступа по ssh
+sudo apt install -y openssh-server
+sudo systemctl start ssh
+sudo systemctl restart ssh
+
+# зависимости для линукса
+sudo apt install -y build-essential libpq-dev unixodbc-dev zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev
+# вспомогательные пакеты и модули
+sudo apt install -y curl wget git htop net-tools docker-compose virtualbox virtualbox-ext-pack
+# основные веб модули
+sudo apt install -y nginx gunicorn python3-dev python3-pip python3-venv postgresql postgresql-contrib mysql-server
+# автоматическая выдача SSL сертификатов
+sudo snap install --classic certbot
+# github CLI
+sudo snap install gh
+# стандартная группа для nginx
+sudo usermod -aG user www-data
+
+
+
+
+
+
+########################################################################################################################
+# POSTGRESQL
+sudo apt update -y
+sudo apt install -y postgresql postgresql-contrib
+sudo passwd postgres
+sudo -i -u postgres
+sudo su - postgres
+createuser django_user
+createdb django_database -O django_user
+psql django_database
+CREATE USER django_user WITH PASSWORD '12345Qwerty!';
+alter user django_user with password '12345Qwerty!';
+
+CREATE DATABASE django_database OWNER django_user;
+GRANT ALL PRIVILEGES ON DATABASE django_database TO django_user;
+
+sudo nano /etc/postgresql/14/main/postgresql.conf
+# listen_addresses = '*'
+sudo nano /etc/postgresql/14/main/pg_hba.conf
+# host    all             all             192.168.0.165/32         scram-sha-256
+# host    all             all             all         scram-sha-256
+
+sudo systemctl stop postgresql
+sudo systemctl status postgresql
+sudo systemctl restart postgresql
+
+
+sudo -i -u postgres
+psql
+\connect django_database
+
+
+CREATE TABLE zarplata ( id serial PRIMARY KEY, username VARCHAR ( 50 ) UNIQUE NOT NULL, salary INT );
+select * from zarplata;
+
+insert into zarplata (username, salary) VALUES ('Bogdan', '60000'), ('Alice', '80000');
+select * from zarplata;
+
+delete from zarplata where username = 'Bogdan';
+select * from zarplata;
+
+########################################################################################################################
+
+
+########################################################################################################################
+# DJANGO
+
+python3 -m pip3 install --upgrade pip
+python3 -m venv env
+
+source env/bin/activate
+python3 -m pip3 install --upgrade pip
+
+pip install wheel
+pip install Django djangorestframework django-cors-headers django-environ django-grappelli djangorestframework-simplejwt gunicorn celery redis psycopg2-binary pyodbc lxml Pillow requests aiohttp beautifulsoup4 openpyxl
+pip install -r requirements.txt
+pip freeze > requirements.txt
+
+django-admin startproject django_settings .
+django-admin startapp django_app
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser --username admin --email bogdandrienko@gmail.com
+
+python manage.py collectstatic --noinput
+python manage.py createcachetable
+python manage.py check --database default
+
+python manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:8000 django_settings.wsgi
+
+########################################################################################################################
+
+
 
 ########################################################################################################################
 WINDOWS 10
